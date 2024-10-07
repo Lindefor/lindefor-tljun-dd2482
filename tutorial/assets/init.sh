0b7823ed662d0135a1b0a03c55e4d142afe5891f
Martin Lindefors <lindefor@kth.se>
2024-10-07 17:33:17 +0200

    Fix minikube and add install time disclaimer

--- tutorial/assets/init.sh
+++ tutorial/assets/init.sh
@@ -9,4 +9,18 @@ nvm use 20 -y
 cd app 
 npm install
 cd ..
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+minikube start --extra-config=kubeadm.ignore-preflight-errors=NumCPU --force --cpus=1
+
+sudo apt update -y
+
+snap install kubectl --classic
+
 clear 
