8f708a8cfa9990526b6be43a0f3195b5dea90e16
TobiasLjunggren <tobistheone@gmail.com>
2024-10-08 15:11:32 +0200

    Test devnull

--- tutorial/assets/init.sh
+++ tutorial/assets/init.sh
@@ -3,37 +3,34 @@
 # Disable command echoing
 set +x
 
-# Redirect all output to /dev/null, so only echo statements are printed
-exec >/dev/null 2>&1
-
 echo "Installing dependencies..."
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/refs/heads/master/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/refs/heads/master/install.sh | bash >/dev/null 2>&1
 
 source ~/.bashrc
-nvm install 20 -y
+nvm install 20 -y >/dev/null 2>&1
 
 cd app
 echo "Installing Node.js packages..."
-npm install
+npm install >/dev/null 2>&1
 cd ..
 
 echo "Installing Minikube... This could take a while"
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo install minikube-linux-amd64 /usr/local/bin/minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 >/dev/null 2>&1
+sudo install minikube-linux-amd64 /usr/local/bin/minikube >/dev/null 2>&1
 
 echo "Adding Kubernetes apt key..."
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - >/dev/null 2>&1
 
 echo "Adding Kubernetes repository..."
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list >/dev/null 2>&1
 
 echo "Starting Minikube with specified configuration..."
-minikube start --extra-config=kubeadm.ignore-preflight-errors=NumCPU --force --cpus=1
+minikube start --extra-config=kubeadm.ignore-preflight-errors=NumCPU --force --cpus=1 >/dev/null 2>&1
 
 echo "Updating package list..."
-sudo apt update -y
+sudo apt update -y >/dev/null 2>&1
 
 echo "Installing kubectl..."
-snap install kubectl --classic
+snap install kubectl --classic >/dev/null 2>&1
 
 echo "Setup complete!"
